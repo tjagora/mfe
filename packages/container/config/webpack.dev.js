1524b86f4f037b61b69616364cd048b80c1e689a
Tatiana Jagora <tatiana.jagora@hearst.com>
2026-06-15 19:09:53 -0400

    Created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -20,10 +20,7 @@ const devConfig = {
                 },
                 shared: packageJson.dependencies, // Share React dependencies to avoid duplication
         }), // Configure Module Federation Plugin as needed
-       new HtmlWebpackPlugin({
-            template: './public/index.html', // Template for the generated HTML file
-       }) 
-    ]
+    ],
 };
 
 module.exports = merge(commonConfig, devConfig);
