b59aadb60908cc22594a8de662a5949726f94e41
Tatiana Jagora <tatiana.jagora@hearst.com>
2026-07-18 16:58:12 -0400

    updated text for Container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
     return <div>
-    <h1>This is Container</h1>
+    <h1>This is Container!!!</h1>
     <hr />
     <MarketingApp />
     </div>
